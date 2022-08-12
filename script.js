71b6a2f5f99f361fe605e2f2bc30e845c6a213bc
Sasha Swan <alexandrio748@gmail.com>
2022-08-12 18:46:26 +0300

    task1

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log('SashaSwan');
+console.log(1);
+console.log('Добро '+'пожаловать '+' на курс');
+// alert(2019);
+// alert(2019-200);
+
+document.getElementById('one').innerHTML = 'Hello World';
+document.getElementById('two').innerHTML = 12*12;
+document.querySelector('.one').innerHTML = 'Hello World';
+document.querySelector('span').innerHTML = 'world';
+document.querySelector('.three').innerHTML = '<h3>Sajanara</h3>';
+
+
+document.querySelector('.four').innerHTML += '<h4>Sajanara</h4> <h3>Babe</h3>';
+
+let a = document.querySelector('.five').innerHTML = 3.1415;
+let div7 = document.querySelector('.seven');
+div7.innerHTML += '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
+
+let z1 = 6;
+let z2 = 3;
+let m = document.querySelector('.multiply').innerHTML = z1*z2;
+
+let y1 = 6;
+let y2 = 3;
+let d = document.querySelector('.divide').innerHTML = y1/y2;
+
+let x1 = 'Hello';
+let x2 = 5;
+let s = document.querySelector('.sum').innerHTML = x1+x2;
+
+let d2 = document.querySelector('.test-2').innerHTML = 5;
+console.log(d2);
+
+let divS3 = document.querySelector('.s3');
+divS3 = document.querySelector('.s4');
+console.log(divS3);
+
+// let b = document.querySelector('body').innerHTML = '';
+
+console.log('test');
+
+
