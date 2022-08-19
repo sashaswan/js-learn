4711b9720777fb41f3f7c53450c85ff74a963618
Sasha Swan <alexandrio748@gmail.com>
2022-08-19 21:15:15 +0300

    task 9

--- script.js
+++ script.js
@@ -1,196 +1,162 @@
-for (let i = 0; i < 6; i++) {
-    console.log(i);
-}
-
-let k = 0;
-while (k < 5) {
-    k++;
-    console.log('k: ' + k);
-}
-
-let sum = 0;
-let p = 0;
-while (p <= 10) {
-    sum = sum + p;
-    p++;
-}
-console.log('sum = ' + sum);
-
-p = 0;
-let out = document.querySelector('.out');
-let flag = 3;
-let outStr = '';
-while (p < 4) {
-    let p1 = 0;
-    while (p1 < 4) {
-        if (p1 < flag) {
-            outStr += '&nbsp';
-        }
-        else {
-            outStr += '*';
-        }
-        p1++;
-    }
-    flag--;
-    outStr += '<br>';
-    p++;
-}
-out.innerHTML = outStr;
-
-// Task 1
-document.querySelector('.b-2').onclick = function t1() {
-    i = 1;
-    let out2 = document.querySelector('.out-2');
-    while (i < 51) {
-        out2.innerHTML += '&nbsp' + i++;
-    }
-}
-// Task 2
-document.querySelector('.b-3').onclick = function t2() {
-    i = 0;
-    let out3 = document.querySelector('.out-3');
-    while (i < 122) {
-        out3.innerHTML += '&nbsp' + `${i += 2}`;
-        out3.innerHTML += '<br>';
-    }
-}
-// Task 3
-document.querySelector('.b-4').onclick = function t3() {
-    i = 25;
-    let out4 = document.querySelector('.out-4');
-    while (i > 6) {
-        out4.innerHTML += '&nbsp' + i;
-        i--;
-    }
-}
-// Task 4
-document.querySelector('.b-5').onclick = function t4() {
-    i = 80;
-    let out5 = document.querySelector('.out-5');
-    while (i > 35) {
-        out5.innerHTML += `${i -= 3}` + '_';
-    }
-}
-// Task 5
-document.querySelector('.b-6').onclick = function t5() {
-    i = 1;
-    let out5 = document.querySelector('.out-6');
-    while (i < 18) {
-        if (i % 2 == 0) {
-            out5.innerHTML += i++ + '_** ';
-        }
-        else {
-            out5.innerHTML += i++ + '_* ';
-        }
+let gas = document.querySelectorAll('.gas');
+for (let i = 0; i < gas.length; i++) {
+    gas[i].onclick = function () {
+        let gallons = document.querySelector('.gallons').value;
+        let amount = this.getAttribute('data');
+        console.log(gallons * amount);
     }
 }
 
-// Task 6
-document.querySelector('.b-7').onclick = function t6() {
-    i = 0;
-    let out6 = document.querySelector('.out-7');
-    while (i < 3) {
-        out6.innerHTML += '******' + '<br>';
-        i++;
-    }
-}
-// Task 7
-
-document.querySelector('.b-8').onclick = function t7() {
-    i8 = document.querySelector('.i-8').value;
-    out7 = document.querySelector('.out-8');
-    while (i8 >= 0) {
-        out7.innerHTML += i8--;
-    }
+document.querySelector('.b-1').onclick = function () {
+    out1 = document.querySelector('.out-1');
+    out1.style.width = '200px';
+    out1.style.height = '40px';
 }
 
-// Task 8
-
-document.querySelector('.b-9').onclick = function t8() {
-    i81 = document.querySelector('.i-81').value;
-    i82 = document.querySelector('.i-82').value;
-    out8 = document.querySelector('.out-9');
-    p = i81;
-    i = i82;
-    if (p < i) {
-        while (i >= i81) {
-            out8.innerHTML += p++ + ' ';
-            i--;
-        }
-    }
+document.querySelector('.b-2').onclick = function () {
+    out2 = document.querySelector('.out-2');
+    out2.classList.add('bg-2');
 }
 
-// Task 10
+document.querySelector('.b-3').onclick = function () {
+    out3 = document.querySelector('.out-3');
+    out3.classList.remove('bg-3');
+}
 
-document.querySelector('.b-10').onclick = function t10() {
-    out10 = document.querySelector('.out-10');
-    i = 1950;
-    while (i < 2001) {
-        if (i % 2 == 0) {
-            out10.innerHTML += i + ' ';
-        }
-        i++;
+document.querySelector('.b-4').onclick = function () {
+    out4 = document.querySelector('.out-4');
+    out4.classList.toggle('bg-4');
+}
+
+document.querySelector('.b-5').onclick = function () {
+    console.log(document.querySelector(".out-5").classList.contains("bg-5"));
+}
+
+document.querySelector('.b-6').onclick = function () {
+    let p = document.querySelectorAll('.p-6').length;
+    document.querySelector('.out-6').innerHTML = p;
+}
+
+document.querySelector('.b-7').onclick = function () {
+    let out7 = document.querySelectorAll('.out-7');
+    for (let i = 0; i < out7.length; i++) {
+        out7[i].classList.add('bg-7');
     }
 }
 
-// Task 11
-
-document.querySelector('.b-11').onclick = function t11() {
-    div = document.querySelectorAll('.div-11');
-    out11 = document.querySelector('.out-11');
-    a = '';
-    i = 0;
-    while (i < div.length) {
-        a += div[i].innerHTML + ' ';
-        i++
-    }
-    out11.innerHTML = a;
-}
-
-// Task 12
-
-document.querySelector('.b-12').onclick = function t12() {
-    div = document.querySelectorAll('.div-12');
-    out12 = document.querySelector('.out-12');
-    i = 0;
-    while (i < div.length) {
-        div[i].style.background = 'orange';
-        i++
-    }
-}
-// Task 13
-document.querySelector('.b-13').onclick = function t13() {
-    i13 = document.querySelectorAll('.i-13');
-    i = 0;
-    while (i < i13.length) {
-        i13[i].value = i + 1;
-        i++;
+document.querySelector('.b-8').onclick = function () {
+    let out8 = document.querySelectorAll('.out-8');
+    for (let i = 0; i < out8.length; i++) {
+        out8[i].classList.toggle('bg-8');
     }
 }
 
-// Task 14
-document.querySelector('.b-14').onclick = function t14() {
-    i14 = document.querySelectorAll('.i-14');
-    out14 = document.querySelector('.out-14');
-    i = 0;
-    newStr = '';
-    while (i < i14.length) {
-        if (i14[i].checked) {
-            newStr += i14[i].value;
-        }
-        i++;
+
+let out9 = document.querySelectorAll('.out-9');
+for (let i = 0; i < out9.length; i++) {
+    out9[i].onclick = function () {
+        out9[i].classList.add('bg-9');
     }
-    out14.innerHTML = newStr;
 }
 
-// Task 15
-document.querySelector('.b-15').onclick = function t15() {
-    out15 = document.querySelector('.out-15');
-    i = 0;
-    newStr = '';
-    while (i < 11) {
-        newStr += (10 - i + ' ' + `${i}` + ' ');
-        i++;
+let out10 = document.querySelectorAll('.out-10');
+for (let i = 0; i < out10.length; i++) {
+    out10[i].onclick = function () {
+        out10[i].classList.toggle('bg-10');
     }
-    out15.innerHTML = newStr;
 }
+
+document.querySelector('.b-12').onclick = function () {
+    let a = document.createElement('div');
+    a.innerHTML += '12';
+    a.classList.add('bg-12');
+    document.querySelector('.out-12').appendChild(a);
+    console.log(a);
+}
+
+document.querySelector('.b-13').onclick = function () {
+    let div = document.createElement('div');
+    let out13 = document.querySelector('.out-13');
+    div.innerText = 'pushMe';
+    div.classList.add('bg-13');
+    out13.append(div);
+    div.onclick = f13_1;
+}
+function f13_1() {
+    document.querySelector('.out-13').innerHTML += this.innerHTML;
+}
+
+document.querySelector('.b-14').onclick = function () {
+    let div = document.createElement('div');
+    let out14 = document.querySelector('.out-14');
+    div.innerText = '14';
+    div.classList.add('bg-14');
+    out14.prepend(div);
+    div.onclick = f13_2;
+}
+function f13_2() {
+    document.querySelector('.out-14').innerHTML += this.innerHTML;
+}
+
+document.querySelector('.b-15').onclick = function () {
+    let div = document.createElement('div');
+    let out15 = document.querySelector('.out-15');
+    div.innerText = '15';
+    div.classList.add('bg-15');
+    out15.before(div);
+    div.onclick = f13_3;
+}
+function f13_3() {
+    document.querySelector('.out-15').innerHTML += this.innerHTML;
+}
+
+document.querySelector('.b-16').onclick = function () {
+    let div = document.createElement('div');
+    let out16 = document.querySelector('.out-16');
+    div.innerText = '16';
+    div.classList.add('bg-16');
+    out16.after(div);
+    div.onclick = f13_4;
+}
+function f13_4() {
+    document.querySelector('.out-16').innerHTML += this.innerHTML;
+}
+
+
+document.querySelector('.b-17').onclick = function () {
+    let div = document.createElement('div');
+    let out17 = document.querySelector('.out-17');
+    div.innerText = '17';
+    div.classList.add('bg-17');
+    out17.replaceWith(div);
+    div.onclick = f13_5;
+}
+function f13_5() {
+    document.querySelector('.out-16').innerHTML += this.innerHTML;
+}
+
+document.querySelector('.b-18').onclick = function () {
+    let a = document.querySelector('.p-18').getAttribute('data');
+    let out18 = document.querySelector('.out-18');
+    out18.innerHTML += a;
+}
+
+document.querySelector('.b-19').onclick = function () {
+    let a = document.querySelectorAll('.p-19');
+    let out19 = document.querySelector('.out-19');
+    for (let i = 0; i < a.length; i++) {
+        out19.innerHTML += a[i].getAttribute('data');
+    }
+}
+
+document.querySelector('.b-20').onclick = function () {
+    let out20 = document.querySelector('.out-20');
+    out20.setAttribute('title','go');
+}
+
+
+
+
+
+
+
