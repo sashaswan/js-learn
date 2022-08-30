1fa8af6f4085e5ae71c39883ac300eedd55181c8
Sasha Swan <alexandrio748@gmail.com>
2022-08-30 22:10:10 +0300

    task 16

--- script.js
+++ script.js
@@ -1,159 +1,185 @@
-let mySet = new Set();
-mySet.add('h');
-mySet.add('b');
-mySet.add('o');
-mySet.add('h');
-console.log(mySet);
+let a1 = [5, 7, 9, 11, 13, 15];
 
-let s2 = new Set();
-const f2 = () => {
-    let i2 = document.querySelector('.i-2').value;
-    s2.add(i2);
-    console.log(s2);
-}
-document.querySelector('.b-2').onclick = f2;
-
-let s3 = new Set(['one', 'two', 'four']);
-
-const f3 = () => {
-    let i3 = document.querySelector('.i-3').value;
-    s3.delete(i3);
-    console.log(s3);
-}
-document.querySelector('.b-3').onclick = f3;
-
-let s4 = new Set(['a', 'b', 'c', 'z']);
-
-const f4 = () => {
-    let i4 = document.querySelector('.i-4');
+function f1() {
     let out = '';
-    if (s4.has(i4.value)) {
-        out += true;
+    for (let i = 0; i < a1.length; i++) {
+        out += a1[i] + ' ';
     }
-    else {
-        out += false;
-    }
-    document.querySelector('.out-4').innerHTML = out;
+    document.querySelector('.out-1').innerHTML = out;
 }
 
-document.querySelector('.b-4').onclick = f4;
+document.querySelector('.b-1').addEventListener('click', f1);
 
-let s5 = new Set(['a', 'b', 'c', 'z', 'a2', 'b2', 'c2', 'z2']);
+let a2 = [5, 7, 9, 11, 13, 15];
 
-const f5 = () => {
+function f2() {
     let out = '';
-    out += s5.size;
+    for (let i = 0; i < a2.length; i++) {
+        out += i + ' ' + a2[i] + ' ';
+    }
+    document.querySelector('.out-2').innerHTML = out;
+}
+
+document.querySelector('.b-2').addEventListener('click', f2);
+
+function f3() {
+    let div = document.getElementsByClassName('out-3');
+    for (let i = 0; i < div.length; i++) {
+        div[i].innerHTML = 3;
+    }
+}
+document.querySelector('.b-3').addEventListener('click', f3);
+
+function f4() {
+    let div = document.querySelectorAll('.out-4');
+    for (let item of div) {
+        item.innerHTML += 4;
+    }
+}
+document.querySelector('.b-4').addEventListener('click', f4);
+
+let a5 = [3, 4, 5, 2, 1, 7, 8, 2, 4, 6, 8, 11, 23, 17];
+
+function f5() {
+    let out = '';
+    for (let item of a5) {
+        if (item > 7) {
+            out += item + ' ';
+        }
+    }
     document.querySelector('.out-5').innerHTML = out;
 }
-document.querySelector('.b-5').onclick = f5;
+document.querySelector('.b-5').addEventListener('click', f5);
 
-let a6 = [1, 2, 3, 4, 5, 3, 4, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56, 4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56];
+let a6 = [[1, 2], [3, 4], [5, 6]];
 
-const f6 = () => {
-    let s6 = new Set(a6);
+function f6() {
+    let arr = [a6];
     let out = '';
-    for (let item of s6) {
+    for (let item of arr) {
         out += item + ' ';
     }
     document.querySelector('.out-6').innerHTML = out;
 }
-document.querySelector('.b-6').onclick = f6;
+document.querySelector('.b-6').addEventListener('click', f6);
 
+let a7 = [{ id: 23, name: 'Ivan' }, { id: 45, name: 'Petr' }];
 
-const f7 = () => {
-    let i7 = document.querySelector('.i-7');
-    let out = '';
-    let arr = i7.value.split('');
-    let set = new Set(arr);
-
-    if (set.size >= 6 && arr.length == set.size) {
-        out += true;
+function f7() {
+    let newObj = {};
+    for (let item of a7) {
+        newObj[item.id] = item.name
     }
-    else {
-        out += false;
-    }
-    document.querySelector('.out-7').innerHTML = out;
+    a7 = newObj;
+    return a7;
 }
-document.querySelector('.b-7').onclick = f7;
+document.querySelector('.b-7').addEventListener('click', () => {
+    console.log(f7());
+});
 
-let s8 = new Set([1, 2, 3, 4, 5, 3, 4, 7, 9, 5, 7, 8, 9, 23, 45, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56, 4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56]);
-let ar8 = [];
-
-const f8 = () => {
-    ar8 = [];
-    for (let item of s8) {
-        if (item > 5) {
-            ar8.push(item);
+let a8 = [{ id: 23, name: 'Ivan' }, { id: 45, name: 'Petr' }];
+function f8() {
+    let arr = [];
+    for (let item of a8) {
+        id = item.id
+        if (typeof id == 'number') {
+            arr.push(id)
         }
     }
-    console.log(ar8);
+    a8 = arr;
+    return a8;
 }
 
-document.querySelector('.b-8').onclick = f8;
+document.querySelector('.b-8').addEventListener('click', () => {
+    console.log(f8());
+});
 
-const f9 = our_set => {
-    let myArr = Array.from(our_set);
-    return myArr;
+let a9 = [[4, 3, 2], [2, 5], [0, 0, 0, 0, 0]];
+
+function f9() {
+    maxStr = a9[0].length
+    for (let i = 0; i < a9.length; i++) {
+        if (a9[i].length > maxStr) {
+            maxStr = a9[i].length
+            return a9[i].length - 1
+        }
+    }
+}
+document.querySelector('.b-9').addEventListener('click', () => {
+    document.querySelector('.out-9').innerHTML = f9();
+});
+
+let a10 = [4, 6, 9, 'Hello'];
+function f10() {
+    let arr = {};
+    for (let key in a10) {
+        arr[key] = a10;
+        return arr;
+    }
 }
 
-document.querySelector('.b-9').onclick = () => {
-    let s9 = new Set([9, 8, 7, 6, 5]);
-    document.querySelector('.out-9').innerHTML = f9(s9);
+document.querySelector('.b-10').addEventListener('click', () => {
+    console.log(f10());
+});
+
+let a11 = {
+    one: 11,
+    two: 7,
+    three: 13,
+    four: 0
 }
 
-const f10 = (out_set, elem) => {
+function f11() {
     let out = '';
-    for (let item of out_set) {
+    for (let key in a11) {
+        if (a11[key] > 10) {
+            out += a11[key] + ' ';
+        }
+    }
+    document.querySelector('.out-11').innerHTML = out;
+}
+
+document.querySelector('.b-11').addEventListener('click', f11);
+
+let a12 = [4, 5, 6, 7];
+
+function f12() {
+    let out = '';
+    for (let item of a12) {
         out += item + ' ';
     }
-    document.querySelector(elem).innerHTML = out;
+    document.querySelector('.out-12').innerHTML = out;
 }
 
-document.querySelector('.b-10').onclick = () => {
-    let a10 = new Set(['4', '5', '6']);
-    f10(a10, '.out-10');
-};
+document.querySelector('.b-12').addEventListener('click', f12);
 
-const f11 = () => {
-    let s = new Set();
-    s.add([1]);
-    s.add([1]);
-    console.log(s);
-}
-
-document.querySelector('.b-11').onclick = f11;
-
-let str12 = 'The name conjures up visions of plum pudding and Christmas punch quaint coaching inns and cozy firesides but also of orphaned and starving children';
-
-const f12 = () => {
-    let arr = str12.split('');
-    let s = new Set(arr)
-    return s;
-}
-
-document.querySelector('.b-12').onclick = () => {
-    console.log(f12());
-}
-
-let str13 = 'Hello ho';
-
-
-const f13 = () => {
-    let arr = Array.from(str13);
-    let newSet = new Set(arr);
-    let clearObj = {};
-    for (let i of newSet) {
-        let count = 0;
-        for (let k in arr) {
-            if (i === arr[k]) {
-                count++;
-            }
-        }
-        clearObj[i] = count;
+let a13 = 'testone';
+function f13() {
+    let out = '';
+    for (let item of a13) {
+        out += item.split() + ' ';
     }
-    return clearObj;
+    document.querySelector('.out-13').innerHTML = out;
 }
 
-document.querySelector('.b-13').onclick = () => {
-    console.log(f13());
+document.querySelector('.b-13').addEventListener('click', f13);
+
+let a14 = new Set([4,5,6]);
+
+function f14() {
+    let out = '';
+    for (let item of a14) {
+        out += item + ' ';
+    }
+    document.querySelector('.out-14').innerHTML = out;
 }
+
+document.querySelector('.b-14').addEventListener('click', f14);
+
+function f15() {
+    let div = document.querySelectorAll('.out-15');
+    for (let item of div) {
+        item.innerHTML = 15;
+    }
+}
+document.querySelector('.b-15').addEventListener('click', f15);
