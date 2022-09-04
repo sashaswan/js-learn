ed4fd569ee99d034f9f9d5bcbbdaccf6836e07c0
Sasha Swan <alexandrio748@gmail.com>
2022-09-04 18:50:39 +0300

    task 20

--- script.js
+++ script.js
@@ -1,146 +1,123 @@
-function t1() {
-    let a = document.querySelector('.div-1').textContent;
-    document.querySelector('.out-1').textContent = a;
-    return a;
+document.querySelector('.i-1').onkeypress = function (event) {
+    let out = '';
+    out += event.key;
+    document.querySelector('.out-1').innerHTML = out;
 }
-document.querySelector('.div-1').onclick = t1;
-
-function t2(e) {
-    let out = e.altKey;
+document.querySelector('.i-2').onkeypress = function (event) {
+    let out = '';
+    out += event.code;
     document.querySelector('.out-2').innerHTML = out;
 }
-document.querySelector('.div-2').onclick = t2;
 
-let w3 = 75;
-
-function t3() {
+document.querySelector('.i-3').onkeypress = function (event) {
     let out = '';
-    let div = document.querySelector('.div-3');
-    out += div.style.width = w3 + 'px';
-    w3 += 5;
-    document.querySelector('.out-3').innerHTML = out;
-}
-document.querySelector('.div-3').onclick = t3;
-
-function t4() {
-    let a = document.querySelector('.div-4').textContent;
-    document.querySelector('.out-4').textContent = a;
-    return a;
-}
-document.querySelector('.div-4').ondblclick = t4;
-
-function t5() {
-    document.querySelectorAll('.div-4');
-    this.classList.toggle('active');
-}
-document.querySelector('.div-5').ondblclick = t5;
-
-function t6() {
-    let ul = document.querySelector('.ul-6');
-    ul.classList.toggle('hide');
-}
-document.querySelector('.div-6').ondblclick = t6;
-
-function t7() {
-    let div = document.querySelector('.div-7');
-    div.classList.toggle('active');
-}
-document.querySelector('.div-7').oncontextmenu = t7;
-
-function t8() {
-    if (document.querySelector('.ch-8').checked) {
-        return false;
+    if (event.charCode <= 57) {
+        out += true;
     }
     else {
-        return true;
+        out += false;
+    }
+    document.querySelector('.out-3').innerHTML = out;
+}
+function t4(event) {
+    let out = document.querySelector('.out-4');
+    let k = event.key;
+
+    if (k.charCodeAt() > 96) {
+        out.innerHTML += k;
     }
 }
-document.querySelector('.ch-8').onchange = () => {
-    oncontextmenu = t8;
+document.querySelector('.i-4').addEventListener('keypress', t4);
+
+function t5(event) {
+    let out = document.querySelector('.out-5');
+    let k = event.key;
+    out.innerHTML += k.toUpperCase();
+}
+document.querySelector('.i-5').addEventListener('keypress', t5);
+
+function t6(event) {
+    let out = document.querySelector('.out-6');
+    let k = event.key;
+    out.innerHTML += k.toLowerCase();
+}
+document.querySelector('.i-6').addEventListener('keypress', t6);
+
+const a7 = ['a', 'z', 'x', 'w', 'y', 't'];
+document.querySelector('.i-7').onkeypress = function (event) {
+    let str = '';
+    let item = a7[Math.floor(Math.random() * a7.length)];
+    str += item;
+    document.querySelector('.out-7').innerHTML += str;
+}
+document.querySelector('.i-8').onkeypress = function (event) {
+    let out = '';
+    if (event.charCode == 105) {
+        out += 1
+    }
+    else if (event.charCode == 111) {
+        out += 0;
+    }
+    else if (event.charCode == 73) {
+        out += 7;
+    }
+    document.querySelector('.out-8').innerHTML += out;
+}
+let out = 0;
+document.querySelector('.i-9').onkeydown = function (event) {
+    if (event.keyCode == 40) {
+        out++
+        return true;
+    }
+    document.querySelector('.out-9').innerHTML = out;
 }
 
-function t9() {
-    document.querySelector('.div-9 img').setAttribute('src', 'img/2.png');
+let imgWidth = document.querySelector('img').clientWidth;
+let imgHeight = document.querySelector('img').clientHeight;
+function t10(event) {
+    if (event.keyCode == 37 || event.keyCode == 39) {
+        imgWidth++
+    }
+    else if (event.keyCode == 38 || event.keyCode == 40) {
+        imgHeight++
+    }
+    document.querySelector('img').style.width = imgWidth + 'px';
+    document.querySelector('img').style.height = imgHeight + 'px';
 }
-document.querySelector('.div-9').oncontextmenu = t9;
-
-let m = document.querySelector('.div-10 img');
-
-m.addEventListener('mouseenter', () => {
-    let div10 = document.querySelector('.div-10 img');
-    div10.src = 'img/2.png';
-})
+document.querySelector('.i-10').onkeydown = t10;
 
 
-let n = document.querySelector('.div-11 img');
-n.addEventListener('mouseenter', () => {
-    let div11 = document.querySelector('.div-11 img');
-    div11.src = 'img/2.png';
-})
-n.addEventListener('mouseleave', () => {
-    let div11 = document.querySelector('.div-11 img');
-    div11.src = 'img/1.png';
-})
-
-let n12 = document.querySelector('.div-12');
-n12.addEventListener('mousedown', () => {
-    n12.classList.toggle('active');
-})
-
-let n13 = document.querySelector('.div-13');
-n13.addEventListener('mousedown', () => {
-    n13.classList.toggle('active');
-})
-
-n13.addEventListener('mouseup', () => {
-    n13.classList.toggle('active');
-})
-
-function t14() {
-    let div = document.querySelector('.div-14');
-    div.classList.toggle('active');
+let keys = [
+    'z',
+    'x',
+    'c',
+    'v',
+    'b',
+    'n',
+    'm'
+]
+function createKeyboard() {
+    let out = '';
+    for (let key of keys) {
+        out += `<div class = 'symb symb-${key}'>${key}</div>`;
+    }
+    document.querySelector('.keyboard').innerHTML = out;
 }
-document.querySelector('.b-14').onclick = t14;
+createKeyboard()
 
+function t11(event) {
+    if (event.type == 'keydown') {
+        document.querySelectorAll('.symb').forEach((symb) => {
+            if (symb.innerHTML == event.key || symb.innerHTML == event.code) {
+                symb.classList.add('active');
+            }
+        });
+    } else if (event.type == 'keyup') {
+        let elem = document.querySelector('.active');
+        elem.classList.remove('active');
+    }
+}
+document.querySelector('.i-11').addEventListener('keyup', t11);
+document.querySelector('.i-11').addEventListener('keydown', t11);
 
-function t15() {
-    let num = +document.querySelector('.div-15').textContent;
-    num++;
-    document.querySelector('.div-15').innerHTML = num;
-}
-document.querySelector('.div-15').onmousemove = t15;
-
-let num = 75;
-function t16() {
-    let div = document.querySelector('.div-16');
-    div.style.width = num + 'px';
-    num++;
-}
-document.querySelector('.div-16').onmousemove = t16;
-
-function t17On() {
-    document.querySelector('.div-16').onmousemove = t16;
-}
-function t17Off() {
-    document.querySelector('.div-16').onmousemove = false;
-}
-document.querySelector('.b-17_on').addEventListener("click", t17On);
-document.querySelector('.b-17_off').addEventListener("click", t17Off);
-
-function t18() {
-    document.querySelector('.div-18').innerHTML = document.querySelector('.div-18').offsetWidth;
-}
-document.querySelector('.div-18').onmouseenter = t18;
-
-
-function t19() {
-    document.querySelector('.div-19').innerHTML = document.querySelector('.div-19').classList;
-}
-document.querySelector('.div-19').onmouseenter = t19;
-let v = 1;
-function t20() {
-    v++; 
-    document.querySelector('progress').value = v;
-}
-document.querySelector('progress').onmousemove = t20;
 
