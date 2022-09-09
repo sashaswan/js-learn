a83550eb7a94a1e20e90112e233250610b4b5ba1
Sasha Swan <alexandrio748@gmail.com>
2022-09-09 22:02:44 +0300

    task 24

--- script.js
+++ script.js
@@ -1,68 +1,55 @@
-function t1() {
-    localStorage.setItem('5', 11);
-    localStorage.getItem('5');
-}
-document.querySelector('.b-1').onclick = t1;
+let key = ['zhrgB3DxC8LoG7Gcilzg'];
 
-function t2() {
-    a2 = [7, 6, 5]
-    localStorage.setItem('a2', JSON.stringify(a2));
-    let arr = localStorage.getItem('a2');
-    arr = JSON.parse(arr);
-}
-document.querySelector('.b-2').onclick = t2;
-
-function t3() {
-    let out = '';
-    let a = localStorage.getItem('a2');
-    a = JSON.parse(a);
-    a.forEach(element => {
-        out += `a2 ${element} <br>`;
+( () => {
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=1`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-1').innerHTML = text;
+        });
     });
-    document.querySelector('.out-3').innerHTML = out;
-}
-document.querySelector('.b-3').onclick = t3;
 
-function t4() {
-    a4 = { hello: 'world', hi: 'mahai' }
-    localStorage.setItem('a4', JSON.stringify(a4));
-    let arr = localStorage.getItem('a4');
-    arr = JSON.parse(arr);
-}
-document.querySelector('.b-4').onclick = t4;
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=2&name=Alex`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-2').innerHTML = text;
+        });
+    });
 
-function t5() {
-    let out = '';
-    let a = localStorage.getItem('a4');
-    a = JSON.parse(a);
-    for (let key in a) {
-        out += key + ': ' + a[key] + '<br>';
-    }
-    document.querySelector('.out-5').innerHTML = out;
-}
-document.querySelector('.b-5').onclick = t5;
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=4&num1=1&num2=100`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-4').innerHTML = text;
+        });
+    });
 
-function t6() {
-    localStorage.clear();
-}
-document.querySelector('.b-6').onclick = t6;
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=5`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-5').innerHTML = text;
+        });
+    });
 
-a7 = [];
-function t7() {
-    let a = document.querySelector('.i-7').value;
-    a7.push(a);
-	localStorage.setItem('a7', JSON.stringify(a7));
-	document.querySelector('.i-7').value = '';
-}
-document.querySelector('.b-7').onclick = t7;
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=6&num1=1&num2=10`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-6').innerHTML = text;
+        });
+    });
 
-function t8() {
-    let a = localStorage.getItem('a7');
-    a = JSON.parse(a);
-    a.pop();
-    localStorage.setItem('a7', a);
-}
-document.querySelector('.b-8').onclick = t8;
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=7`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-7').innerHTML = `<img src="${text}" style="width:80px">`;
+        });
+    });
+
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=8&year=2001`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-8').innerHTML = text;
+        });
+    });
+
+    fetch(`http://localhost:8888/index2.php?auth=${key}&action=9&m=1&d=1&y=1`).then(function (response) {
+        response.text().then(function (text) {
+            document.querySelector('.out-9').innerHTML = text;
+        });
+    });
+    
+} )();
 
 
-	
+
