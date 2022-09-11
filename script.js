a8487961af86c8175538cee1ea06deafb540e9e9
Sasha Swan <alexandrio748@gmail.com>
2022-09-11 21:35:48 +0300

    task 26

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-
-
-let xhttp = new XMLHttpRequest();
-
 let key = ['zhrgB3DxC8LoG7Gcilzg'];
-
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction(this.responseText)
-    }
-}
-
-let action = [
+let getData = [
     'action=1',
     'action=2&name=Alex',
     'action=3&num1=10&num2=10',
@@ -22,78 +11,81 @@ let action = [
     'action=9&m=1&d=1&y=1',
 ]
 
+document.querySelector('.b-1').onclick = () => {
+    out = ['.out-1']
+    data = [getData[0]];
+    f1(out, data);
+}
+
+
+document.querySelector('.b-2').onclick = () => {
+    out = ['.out-2']
+    data = [getData[1]];
+    f1(out, data);
+}
+
+document.querySelector('.b-3').onclick = () => {
+    out = ['.out-3']
+    data = [getData[2]];
+    f1(out, data);
+}
+
+document.querySelector('.b-4').onclick = () => {
+    out = ['.out-4']
+    data = [getData[3]];
+    f1(out, data);
+}
+
+document.querySelector('.b-5').onclick = () => {
+    out = ['.out-5']
+    data = [getData[4]];
+    f1(out, data);
+}
+
+document.querySelector('.b-6').onclick = () => {
+    out = ['.out-6']
+    data = [getData[5]];
+    f1(out, data);
+}
+
+document.querySelector('.b-7').onclick = () => {
+    out = ['.out-7']
+    data = [getData[6]];
+    f2(out, data);
+}
+
+document.querySelector('.b-8').onclick = () => {
+    out = ['.out-8']
+    data = [getData[7]];
+    f1(out, data);
+}
+
+document.querySelector('.b-9').onclick = () => {
+    out = ['.out-9']
+    data = [getData[8]];
+    f1(out, data);
+}
+
 function f1() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[0]}`, true);
-    xhttp.send();
+    fetch(`http://localhost:8888/index2.php?auth=${key}&${data}`)
+        .then(data => data.text())
+        .then(data => {
+            document.querySelector(out).innerHTML = data;
+        })
 }
-document.querySelector('.b-1').onclick = () => f1(out = ['.out-1']);
-
 function f2() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[1]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-2').onclick = () => f2(out = ['.out-2']);
-
-
-function f3() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[2]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-3').onclick = () => f3(out = ['.out-3']);
-
-function f4() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[3]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-4').onclick = () => f4(out = ['.out-4']);
-
-function f5() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[4]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-5').onclick = () => f5(out = ['.out-5']);
-
-function f6() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[5]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-6').onclick = () => f6(out = ['.out-6']);
-
-function f7() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[6]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-7').onclick = () => f7(out = ['.out-7']);
-
-function f8() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[7]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-8').onclick = () => f8(out = ['.out-8']);
-
-function f9() {
-    xhttp.open("GET", `http://localhost:8888/index2.php?auth=${key}&${action[8]}`, true);
-    xhttp.send();
-}
-document.querySelector('.b-9').onclick = () => f9(out = ['.out-9']);
-
-function f11() {
-    xhttp.open("POST", `http://localhost:8888/index2.php?auth=${key}&${action[1]}`, true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send('auth=zhrgB3DxC8LoG7Gcilzg&action=1')
-}
-document.querySelector('.b-11').onclick = () => f11(out = ['.out-11']);
-
-function f12() {
-    xhttp.open("POST", `http://localhost:8888/index2.php?auth=${key}&${action[2]}`, true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send('auth=zhrgB3DxC8LoG7Gcilzg&action=1')
-}
-document.querySelector('.b-12').onclick = () => f12(out = ['.out-12']);
-
-function myFunction(data) {
-    document.querySelector(out).innerHTML = data;
-    if (data.indexOf('https') > -1) {
-        document.querySelector('.out-7').innerHTML = `<img src='${data}' style='width: 50px'>`;
-    }
+    fetch(`http://localhost:8888/index2.php`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `auth=${key}&${data}`,
+    })
+        .then(response => response.text())
+        .then(response => {
+            document.querySelector(out).innerHTML = response;
+            if (response.indexOf('https') > -1) {
+                document.querySelector(out).innerHTML = `<img src="${response}" style="width:50px;">`
+            }
+        })
 }
